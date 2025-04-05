8b8791e2b09ef4734f2b6c6572db6c0f0291a278
Harun Goralija <harun.goralija@gmail.com>
2025-04-05 16:22:31 +0200

    v1: fb auth

--- apps/buyer/app/(auth)/login.tsx
+++ apps/buyer/app/(auth)/login.tsx
@@ -12,6 +12,13 @@ import {
 import { useRouter } from 'expo-router';
 import { FontAwesome } from '@expo/vector-icons';
 import { useTranslation } from 'react-i18next';
+import {
+  AccessToken,
+  LoginButton,
+  Settings,
+  Profile,
+  LoginManager
+} from "react-native-fbsdk-next";
 
 export default function SignIn() {
   const router = useRouter();
@@ -22,6 +29,31 @@ export default function SignIn() {
 
   const toggleLanguage = () => {
     i18n.changeLanguage(i18n.language === 'en' ? 'bs' : 'en');
+  };
+
+  const loginWithFacebook = () => {
+    LoginManager.logInWithPermissions(["public_profile", "email"]).then(
+      function (result) {
+        if (result.isCancelled) {
+          console.log("==> Login cancelled");
+        } else {
+          console.log(result);
+          AccessToken.getCurrentAccessToken().then((data) => {
+            console.log(data);
+            getUserFBData();
+          });
+        }
+      },
+      function (error) {
+        console.log("==> Login fail with error: " + error);
+      }
+    );
+  };
+
+  const getUserFBData = () => {
+    Profile.getCurrentProfile().then((currentProfile) => {
+      console.log(currentProfile);
+    });
   };
 
   const onSignInPress = async () => {
@@ -99,7 +131,7 @@ export default function SignIn() {
         <Text style={styles.socialButtonText}>{t('login_google')}</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.socialButton}>
+      <TouchableOpacity style={styles.socialButton} onPress={() => {loginWithFacebook();}}>
         <FontAwesome name="facebook" size={20} color="#1877F2" />
         <Text style={styles.socialButtonText}>{t('login_facebook')}</Text>
       </TouchableOpacity>
