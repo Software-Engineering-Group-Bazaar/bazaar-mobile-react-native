a33d1ee35732aca7e01d88feae8e1572784a070d
Zlatan Jakic <zlatanjakic@Zlatans-MacBook-Air.local>
2025-04-21 17:48:17 +0200

    Refactor pregled_prodavnice.tsx

--- apps/seller/app/(tabs)/pregled_prodavnica.tsx
+++ apps/seller/app/(tabs)/pregled_prodavnica.tsx
@@ -1,36 +1,31 @@
-import {
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  ActivityIndicator,
-  ScrollView,
-} from "react-native";
+import { View, ScrollView, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import { useTranslation } from "react-i18next";
-import { FontAwesome } from "@expo/vector-icons";
-import styles from "../styles";
 import React, { useState, useEffect } from "react";
-import { apiFetchActiveStores } from "../api/storeApi";
+import { apiFetchActiveStore } from "../api/storeApi";
 import SetHeaderRight from "../../components/ui/NavHeader";
 import { Store } from "../types/prodavnica";
 import LanguageButton from "@/components/ui/LanguageButton";
 import CreateButton from "@/components/ui/CreateButton";
+import TouchableCard from "@/components/ui/cards/TouchableCard";
 
 export default function StoresScreen() {
   const router = useRouter();
   const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
-  const [stores, setStores] = useState<Store[]>([]);
+  const [store, setStore] = useState<Store>();
 
   useEffect(() => {
-    async function getStores() {
+    async function getStore() {
       setLoading(true);
-      const activeStores = await apiFetchActiveStores();
-      setStores(activeStores);
+      const activeStore = await apiFetchActiveStore();
+      if (activeStore) {
+        setStore(activeStore);
+      }
       setLoading(false);
+      console.log(activeStore);
     }
-    getStores();
+    getStore();
   }, []);
 
   const handleCreateStore = () => {
@@ -40,38 +35,6 @@ export default function StoresScreen() {
   const handleViewOrders = () => {
     router.push("../(CRUD)/pregled_narudzbi");
   };
-
-  const renderStoreCard = ({ item }: { item: Store }) => (
-    <>
-      <TouchableOpacity
-        style={styles.section}
-        onPress={() =>
-          router.push(
-            `/(CRUD)/prodavnica_detalji?store=${JSON.stringify(item)}`
-          )
-        }
-      >
-        <View style={styles.storeInfo}>
-          <Text style={styles.storeName}>{item.name}</Text>
-          <Text style={styles.storeAddress}>{item.categoryName}</Text>
-          <Text style={styles.storeAddress}>{item.description}</Text>
-          {/* <Text style={styles.storeAddress}>{item.address}</Text> */}
-        </View>
-      </TouchableOpacity>
-
-      {/* Sekcija: Pregled narudžbi */}
-      <View style={styles.section}>
-        <TouchableOpacity style={styles.storeCard} onPress={handleViewOrders}>
-          <View style={styles.storeInfo}>
-            <Text style={styles.storeName}>{t("view_orders")}</Text>
-            <Text style={styles.storeAddress}>
-              {t("view_orders_description") || "View all orders for this store"}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </>
-  );
 
   return (
     <View style={{ flex: 1 }}>
@@ -84,29 +47,45 @@ export default function StoresScreen() {
         showsVerticalScrollIndicator={true}
       >
         <View style={[styles.container, { paddingTop: 40 }]}>
-          <View style={styles.titleSpacing} />
-          <Text style={styles.title}>{t("my_stores")}</Text>
-
-          {stores.length === 0 && (
+          {store == null ? (
             <CreateButton
               text={t("add_a_new_store")}
               onPress={handleCreateStore}
               loading={loading}
             />
+          ) : (
+            <View style={styles.cardsContainer}>
+              <TouchableCard
+                title={store.name}
+                textRows={[store.categoryName, store.description]}
+                onPress={() =>
+                  router.push(
+                    `/(CRUD)/prodavnica_detalji?store=${JSON.stringify(store)}`
+                  )
+                }
+              />
+              <TouchableCard
+                title={t("view_orders")}
+                textRows={[
+                  t("view_orders_description") ||
+                    "View all orders for this store",
+                ]}
+                onPress={handleViewOrders}
+              />
+            </View>
           )}
-
-          <FlatList
-            data={stores}
-            renderItem={renderStoreCard}
-            keyExtractor={(item: Store) => item.id.toString()}
-            contentContainerStyle={[
-              styles.listContainer,
-              { paddingBottom: 100 },
-            ]}
-            scrollEnabled={false}
-          />
         </View>
       </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  cardsContainer: {
+    padding: 16,
+  },
+});
