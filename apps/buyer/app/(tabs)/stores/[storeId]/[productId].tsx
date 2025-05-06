0fe00d61c3f97a424cde3d508a46ce8a55a8c2a9
kazaz03 <akazazovic1@etf.unsa.ba>
2025-05-06 19:18:04 +0200

    dodane prepravke

--- apps/buyer/app/(tabs)/stores/[storeId]/[productId].tsx
+++ apps/buyer/app/(tabs)/stores/[storeId]/[productId].tsx
@@ -151,7 +151,7 @@ const checkAndAddToCart = async () => {
     } else {
       Alert.alert(
         t('out of stock'),
-        `${t('You can only add up to')} ${availableQuantity - currentQuantityInCart} ${t('more of')} ${product.name}. ${t('Your current cart contains')} ${currentQuantityInCart} ${t('items of this product.')}`
+        t('Cannot add that many items. Not enough stock available.')
       );      
     }
   } catch (error) {
