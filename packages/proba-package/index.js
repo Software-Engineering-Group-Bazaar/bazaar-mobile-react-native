1d5e10932a94a02660732d7afc8c85741312e1a3
lborovina1 <lborovina1@etf.unsa.ba>
2025-05-18 18:44:31 +0200

    i dalje povlaci buyer?

--- packages/proba-package/index.js
+++ packages/proba-package/index.js
@@ -1,8 +1,2 @@
-export const greeting = 'Hello!';
-
-export const baseURL ='https://bazaar-system.duckdns.org';  // 'http://192.168.0.25:5054'; // 'http://10.242.191.189:5054';
-export const USE_DUMMY_DATA = false;
-
-
-import '../../apps/seller/app/src/i18n/i18n.config'
 import '../../apps/buyer/app/src/i18n/i18n.config'
+import '../../apps/seller/app/src/i18n/i18n.config'
