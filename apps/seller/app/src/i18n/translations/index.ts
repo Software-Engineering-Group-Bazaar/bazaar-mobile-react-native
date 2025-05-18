87bfb161e2b45e861e8e0df5b24be54abe1a8327
aminacajic <acajic1@etf.unsa.ba>
2025-05-18 19:34:21 +0200

    i18n problem4

--- apps/seller/app/src/i18n/translations/index.ts
+++ apps/seller/app/src/i18n/translations/index.ts
@@ -1,4 +1,4 @@
 import bs from '../../../../../seller/app/src/i18n/translations/bs.json'
-import en from '../../../../../seller/app/src/i18n/translations/bs.json'
+import en from '../../../../../seller/app/src/i18n/translations/en.json'
 
 export { en, bs };
