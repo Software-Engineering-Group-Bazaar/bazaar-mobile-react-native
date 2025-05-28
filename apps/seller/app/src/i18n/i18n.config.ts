971bca90aad3209a1e4c9ffc6b98150142f7d1d4
Benjamin Bandić <bbandic1@etf.unsa.ba>
2025-05-28 19:20:51 +0200

    Just added the translation files for de and es

--- apps/seller/app/src/i18n/i18n.config.ts
+++ apps/seller/app/src/i18n/i18n.config.ts
@@ -2,19 +2,27 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import {en} from "../../../../seller/app/src/i18n/translations";
 import {bs} from "../../../../seller/app/src/i18n/translations";
+import {de} from "../../../../seller/app/src/i18n/translations";
+import {es} from "../../../../seller/app/src/i18n/translations";
 
 const resources = {
   en: {
-    translation: bs,
-  },
-  bs: {
     translation: en,
   },
+  bs: {
+    translation: bs,
+  },
+  de: {
+    translation: de,
+  },
+  es: {
+    translation: es
+  }
 };
 
 i18next.use(initReactI18next).init({
   debug: true,
-  lng: "en",
+  lng: "bs",
   compatibilityJSON: "v4",
   //language to use if translation in user language is not available
   fallbackLng: "en",
