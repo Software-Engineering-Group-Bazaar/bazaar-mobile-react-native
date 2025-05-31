56218c15768882f637d52dbc58b5fc1fcae1c405
Zlatan Jakic <zlatanjakic@Zlatans-MacBook-Air.local>
2025-05-31 13:21:31 +0200

    Nisam bio savea login

--- apps/seller/app/(auth)/login.tsx
+++ apps/seller/app/(auth)/login.tsx
@@ -11,7 +11,6 @@ import {
 } from "@react-native-google-signin/google-signin";
 import { apiLogin, fbLoginApi } from "../api/auth/loginApi";
 import InputField from "@/components/ui/input/InputField";
-import LanguageButton from "@/components/ui/buttons/LanguageButton";
 import SubmitButton from "@/components/ui/input/SubmitButton";
 import HelpAndLanguageButton from "@/components/ui/buttons/HelpAndLanguageButton";
 
